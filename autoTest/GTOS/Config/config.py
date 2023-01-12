ea2c3cc9682af33abb8463e5bc2501dc1b968794
kkstar <402786629@qq.com>
2023-01-12 11:12:26 +0800

    update

--- autoTest/GTOS/Config/config.py
+++ autoTest/GTOS/Config/config.py
@@ -4,6 +4,10 @@ host="http://10.166.0.155/app/#/login"
 #进场计划箱号
 global boxNumber
 #boxNumber=CommonGenerator.generate_spec("JXNU",7)
+global importNumber
+global outportNumber
+importNumber="I0001"
+outportNumber="E0001"
 boxNumber="JXNU000020"
 takeNumber="1014" #进箱提单号
 username="admin"
