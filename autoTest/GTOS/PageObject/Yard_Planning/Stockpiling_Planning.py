5cd5a0b466e68fc52af5891eb7676cf1a31b7736
kkstar <402786629@qq.com>
2023-01-28 13:59:13 +0800

    update

--- autoTest/GTOS/PageObject/Yard_Planning/Stockpiling_Planning.py
+++ autoTest/GTOS/PageObject/Yard_Planning/Stockpiling_Planning.py
@@ -13,31 +13,16 @@ class Stockpiling_Planning(BasePage):
     """
     堆存计划
     """
-    def select_values_into(self,input):
+
+    def search(self, type,number):
         """
         选择计划类型，船名航次,道口进
         """
         self.logger.info('步骤1：选择计划类型，船名航次')
         textInput = Gtos_text(self.driver)
-        textInput.search_select_by_label('计划类型','道口进')
-        textInput.search_select_by_label('船名航次',config.outportNumber)
-
-    def select_values_out(self,input):
-        """
-        选择计划类型，船名航次,卸船
-        """
-        self.logger.info('步骤1：选择计划类型，船名航次')
-        textInput = Gtos_text(self.driver)
-        textInput.search_select_by_label('计划类型','卸船')
-        textInput.search_select_by_label('船名航次',config.importNumber)
-
-    def Retrieval(self):
-        """
-        检索
-        """
-        self.logger.info('步骤2：点击检索')
-        self.click('x',"(//span[text()='检索'])[1]")
-
+        textInput.search_select_by_label('计划类型', type)
+        textInput.search_select_by_label('船名航次', number)
+        self.click('x', "(//span[text()='检索'])[1]")
 
     def Add_into_plan(self):
         """
@@ -66,25 +51,19 @@ class Stockpiling_Planning(BasePage):
         textInput.click('x',"//span[text()='保存']")
         self.check_alert('新增成功')
 
-
-
-
-
-    def process_into(self,input):
+    def process_into(self):
         """
         流程
         """
-        self.select_values_into(input)
-        self.Retrieval()
+        self.search("道口进",config.outportNumber)
         self.close_alert('未找到相关堆存计划')
         self.Add_into_plan()
         self.Add_box()
 
 
-    def process_out(self,input):
+    def process_out(self):
         self.refresh()
-        self.select_values_out(input)
-        self.Retrieval()
+        self.search("卸船",config.importNumber)
         self.close_alert('未找到相关堆存计划')
         self.Add_into_plan()
         self.Add_box()
