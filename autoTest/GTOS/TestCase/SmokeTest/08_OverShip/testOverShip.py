dc34044e923f1d9be9443bc21201b0cc2b062d9f
kkstar <402786629@qq.com>
2023-01-29 10:44:52 +0800

    delete bigship

--- autoTest/GTOS/TestCase/SmokeTest/08_OverShip/testOverShip.py
+++ autoTest/GTOS/TestCase/SmokeTest/08_OverShip/testOverShip.py
@@ -7,15 +7,12 @@ from GTOS.Controls.Gtos_table import Gtos_table
 from GTOS.PageObject.DataManagement.closeFlight import CloseFlight
 from GTOS.PageObject.gtos_menu import GtosMenu
 from GTOS.PageObject.Barge_Planning.Immediate_Plan import Immediate_plan
-from GTOS.PageObject.Barge_Planning.Bridge_Crane_distribution import Bridge_Crane_Distribution
-from GTOS.PageObject.Barge_Planning.Voyage_Attached import Voyage_Attached
-from GTOS.PageObject.Yard_Planning.Stockpiling_Planning import Stockpiling_Planning
 from GTOS.PageObject.Control_Ship.No_Structure_Monitoring import NO_Structure_Monitoring
 from Commons.yamlread import read_yaml
 
 
 @allure.title('1、吊桥完工')
-@allure.story('1.离泊确认，航次关闭')
+@allure.story('1.离泊确认,航次关闭')
 def testShip_operation(driver):
     """无结构靠泊、吊桥分配"""
     menu = GtosMenu(driver)
@@ -32,7 +29,7 @@ def testShip_operation(driver):
 
 @pytest.mark.parametrize("input",read_yaml(os.path.join(os.getcwd(),'01_DataProcess', 'immediata_plan.yaml')))
 @allure.title('2、近期计划验证靠泊信息')
-@allure.story('1.驳船流程功能准备')
+@allure.story('1.离泊确认,航次关闭')
 def testCheck_shipinfo(driver, input):
     """近期计划验证靠泊信息"""
     menu = GtosMenu(driver)
