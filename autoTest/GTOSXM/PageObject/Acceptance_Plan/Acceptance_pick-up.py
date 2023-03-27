90c4d111996afcb5b604bc9d65f66bbcf91cb335
LAPTOP-DGIFU26M\admin <123123>
2023-03-27 15:03:39 +0800

    GTOS 无用文件删除

--- autoTest/GTOSXM/PageObject/Acceptance_Plan/Acceptance_pick-up.py
+++
@@ -1,9 +0,0 @@
-import time
-import pytest_check as check
-import selenium.webdriver.common.log
-from selenium.webdriver import ActionChains, Keys
-
-from Base.basepage import BasePage
-from GTOS.Controls.text import Gtos_text
-from GTOS.Config import config
-from GTOS.Controls.Gtos_table import Gtos_table
