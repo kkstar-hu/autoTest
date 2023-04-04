3d63887a73e52c1b6c3146b2753a32d96cb48e51
yuhua-333 <1048654477@qq.com>
2023-04-04 16:27:25 +0800

    BTOS-理货员出勤

--- autoTest/BTOSLJ/PageObject/tallyman/tallyman_manage.py
+++ autoTest/BTOSLJ/PageObject/tallyman/tallyman_manage.py
@@ -1,3 +1,5 @@
+import time
+
 from BTOSLJ.Controls.BTOS_table import BTOS_table
 from BTOSLJ.Controls.BTOS_text import BtosText
 from Base.basepage import BasePage
@@ -13,7 +15,11 @@ class Tallyman(BasePage):
         self.textInput.input_by_label("工作日期", self.work_date)
         self.textInput.select_by_label("工班", input["工班"])
         self.left_click('x', "//span[text()=' 检索 ']")
+        time.sleep(0.5)
+        if self.has_alert("没有符合条件的已安排理货员"):
+            self.close_alert("没有符合条件的已安排理货员")
         self.tableInput.check("工号", input['工号值'])
-        self.left_click('x', "//i[@class='el-icon-arrow-right']")
-        self.left_click('x', "//span[text()=' 保存 ']")
-        self.check_alert("保存成功")
+        if self.elementExist('x', "(//table[@class='vxe-table--body'])[1]//tr[@rowid='row_19']/td//span[@class='vxe-cell--checkbox']"):
+            self.left_click('x', "//i[@class='el-icon-arrow-right']")
+            self.left_click('x', "//span[text()=' 保存 ']")
+            self.check_alert("保存成功")
