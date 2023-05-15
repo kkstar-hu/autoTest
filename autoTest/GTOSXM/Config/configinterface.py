d6ee0eb40a89512118c8c4836906c9f1205d382d
Qizh <123123>
2023-05-15 17:44:30 +0800

    config内容整合

--- autoTest/GTOSXM/Config/configinterface.py
+++ autoTest/GTOSXM/Config/configinterface.py
@@ -1,21 +1,28 @@
-loginurl = 'http://apserver.cloudtos-xm.com/api/auth'
-url = 'http://apserver.cloudtos-xm.com'
-BodyXICT = {
-    "useraccount" : "admin",
-    "password" : "77B7707169951B7246393E8395FB656C",
-    "ExtendProperties": {"OP_TERMCD": "XICT"}
-}
-
-BodyXRCT = {                                         # β环境账号密码
-    "useraccount" : "autotest",
-    "password" : "77B7707169951B7246393E8395FB656C",
-    "ExtendProperties" : {"OP_TERMCD":"XRCT"}
-}
+from GTOSXM.Config import config
 
 head = {
     "Authorization": "Bearer",
     "Content-Type": "application/json; charset=UTF-8"
 }
+
+if 'xm' in config.host:
+    loginurl = 'http://apserver.cloudtos-xm.com/api/auth'
+    url = 'http://apserver.cloudtos-xm.com'
+    # β环境账号密码
+    BodyXRCT = {
+        "useraccount": "autotest",
+        "password": "77B7707169951B7246393E8395FB656C",
+        "ExtendProperties": {"OP_TERMCD": "XRCT"}
+    }
+else:
+    loginurl = 'http://apserver.cloudtos.com/api/auth'
+    url = 'http://apserver.cloudtos.com'
+    # α环境账号密码
+    BodyXRCT = {
+        "useraccount" : "admin",
+        "password" : "77B7707169951B7246393E8395FB656C",
+        "ExtendProperties": {"OP_TERMCD": "XRCT"}
+    }
 
 global boxNumber
 # boxNumber = 'DP123'
