17c5cbc2f5ee7cfd2c8d1947442d523c96f54bcb
Qizh <123123>
2023-05-16 16:41:24 +0800

    α、β地址切换，自动匹配账号密码，接口地址

--- autoTest/NZYMS/config/config.py
+++ autoTest/NZYMS/config/config.py
@@ -4,11 +4,11 @@ host="http://10.126.0.89:7980/#/login"
 #进场计划箱号
 global boxNumber
 boxNumber=CommonGenerator.generate_spec("JXNU",7)
-#boxNumber="JXNU7489305"
+# boxNumber="JXNU4000000"
 
 global boxNumberOutPlan
-boxNumberOutPlan=CommonGenerator.generate_spec("JXNU",7)
-#boxNumberOutPlan="JXNU7489305"
+# boxNumberOutPlan=CommonGenerator.generate_spec("CZXZ",7)
+boxNumberOutPlan="CZXZ3016897"
 
 global planNumber
 #查询箱信息内容，字典模式
@@ -17,7 +17,6 @@ global dict
 global outplanNumber
 #散货入库计划号
 global bulkintoNumber
-#bulkintoNumber='WIP23030200011'
 #散货出库计划号
 global bulkoutNumber
 #查验计划号
@@ -26,7 +25,7 @@ global checkplanNumber
 global packingboxNumber
 #拆箱计划好
 global splitboxNumber
-username=9540
-password="861310Aa"
-showname="胡康莉"
-createName="8540/胡康莉"
+username='9541'
+password="q11111111@"
+showname="祁洲海"
+createName="8541/祁洲海"
